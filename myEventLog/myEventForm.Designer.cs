6feb4c0756b6aee07b245a7e91883d09946ff956
jwhwan9 <jwhwang@gmail.com>
2017-10-30 15:41:40 +0800

    Add files via upload
    GUI to Get Windows Eventlog content

---
+++ myEventLog/myEventForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace myEventLog
+{
+    partial class myEventForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ddlServer = new System.Windows.Forms.ComboBox();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.cmdQuery = new System.Windows.Forms.Button();
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ddlEventType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numItems = new System.Windows.Forms.NumericUpDown();
+            this.dtMax = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pgBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ddlServer
+            // 
+            this.ddlServer.FormattingEnabled = true;
+            this.ddlServer.Items.AddRange(new object[] {
+            "./",
+            "MyWinPC"});
+            this.ddlServer.Location = new System.Drawing.Point(12, 48);
+            this.ddlServer.Name = "ddlServer";
+            this.ddlServer.Size = new System.Drawing.Size(121, 20);
+            this.ddlServer.TabIndex = 0;
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(267, 49);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(238, 22);
+            this.txtFilter.TabIndex = 1;
+            // 
+            // cmdQuery
+            // 
+            this.cmdQuery.Location = new System.Drawing.Point(523, 47);
+            this.cmdQuery.Name = "cmdQuery";
+            this.cmdQuery.Size = new System.Drawing.Size(75, 23);
+            this.cmdQuery.TabIndex = 2;
+            this.cmdQuery.Text = "Search";
+            this.cmdQuery.UseVisualStyleBackColor = true;
+            this.cmdQuery.Click += new System.EventHandler(this.cmdQuery_Click);
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Location = new System.Drawing.Point(604, 48);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(75, 23);
+            this.cmdExit.TabIndex = 3;
+            this.cmdExit.Text = "Exit";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 89);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1324, 641);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // ddlEventType
+            // 
+            this.ddlEventType.FormattingEnabled = true;
+            this.ddlEventType.Items.AddRange(new object[] {
+            "電子商務",
+            "Application",
+            "System",
+            "Security"});
+            this.ddlEventType.Location = new System.Drawing.Point(140, 47);
+            this.ddlEventType.Name = "ddlEventType";
+            this.ddlEventType.Size = new System.Drawing.Size(121, 20);
+            this.ddlEventType.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Max Count:";
+            // 
+            // numItems
+            // 
+            this.numItems.Location = new System.Drawing.Point(427, 22);
+            this.numItems.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numItems.Name = "numItems";
+            this.numItems.Size = new System.Drawing.Size(76, 22);
+            this.numItems.TabIndex = 8;
+            // 
+            // dtMax
+            // 
+            this.dtMax.Location = new System.Drawing.Point(270, 22);
+            this.dtMax.Name = "dtMax";
+            this.dtMax.Size = new System.Drawing.Size(149, 22);
+            this.dtMax.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "查詢最後時間:";
+            // 
+            // pgBar
+            // 
+            this.pgBar.Location = new System.Drawing.Point(523, 18);
+            this.pgBar.Name = "pgBar";
+            this.pgBar.Size = new System.Drawing.Size(156, 23);
+            this.pgBar.TabIndex = 11;
+            this.pgBar.UseWaitCursor = true;
+            // 
+            // myEventForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 742);
+            this.Controls.Add(this.pgBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtMax);
+            this.Controls.Add(this.numItems);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ddlEventType);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.cmdQuery);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.ddlServer);
+            this.Name = "myEventForm";
+            this.Text = "myEventForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numItems)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ddlServer;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Button cmdQuery;
+        private System.Windows.Forms.Button cmdExit;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox ddlEventType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numItems;
+        private System.Windows.Forms.DateTimePicker dtMax;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar pgBar;
+    }
+}
+
---
+++ myEventLog/myEventForm.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security;
+using System.Threading;
+using System.Management;
+
+namespace myEventLog
+{
+    public partial class myEventForm : Form
+    {
+        static int MAX_EventCount = 100;
+        static bool isExit = false;
+        public myEventForm()
+        {
+            InitializeComponent();
+            InitDateTimePicker();
+            InitList();
+
+            //this.dtMax.Format = DateTimePickerFormat.Time;
+            //this.dtMax.ShowUpDown = true;
+            numItems.Value = MAX_EventCount;
+        }
+
+        void InitDateTimePicker()
+        {
+
+            dtMax.Format = DateTimePickerFormat.Custom;
+            dtMax.CustomFormat = "MM dd yyyy hh mm ss"; 
+        }
+
+        void InitList()
+        {
+            listView1.Columns.Add("No.", 50, HorizontalAlignment.Center);
+            listView1.Columns.Add("Machine", 80, HorizontalAlignment.Center);
+            listView1.Columns.Add("RecordNumber", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("TimeWritten", 150, HorizontalAlignment.Center);
+            listView1.Columns.Add("SourceName", 200, HorizontalAlignment.Left);
+            listView1.Columns.Add("Message", 600, HorizontalAlignment.Left);
+
+            ddlEventType.SelectedIndex = 0;
+            ddlServer.SelectedIndex = 1;
+        }
+
+        void performSearch()
+        {
+
+            // Set cursor as hourglass
+            //this.UseWaitCursor = true;
+            //listView1.BeginUpdate();
+
+            //gatherEvent(ddlServer.Text, txtFilter.Text.Trim(), ddlEventType.SelectedItem.ToString().Trim());
+            /*
+            try
+            {
+                gatherEvent();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+             */
+            //listView1.EndUpdate();
+            // Set cursor as default arrow
+            //this.UseWaitCursor = false;
+
+        }
+
+        void gatherEvent(string Machine_Name, string Keyword_Text, string Event_Type)
+        {
+
+            string machine = Machine_Name; //ddlServer.Text; 
+            string keyword = Keyword_Text; //txtFilter.Text.Trim();
+            string eventType = Event_Type; //ddlEventType.SelectedItem.ToString().Trim();
+
+            //MessageBox.Show( machine);            
+            EventLog aLog = new EventLog(eventType, machine);
+            EventLogEntry entry;
+            EventLogEntryCollection entries = aLog.Entries;
+
+            int LastLogToShow = entries.Count;
+            if (LastLogToShow <= 0)
+            {
+                //Console.WriteLine("No Event Logs in the Log :" + eventType);
+            }
+            else
+            {
+
+                // Progress Bar
+                pgBar.Minimum = 0;
+                pgBar.Maximum = (int)numItems.Value;
+
+                for (int i = entries.Count - 1; i > 0 && listView1.Items.Count < numItems.Value; i--)
+                {
+                    if (isExit)
+                    {
+                        return;
+                    }
+
+                    entry = entries[i];
+
+                    if (i == 0) break;
+
+                    if (entry.Message.IndexOf(keyword) > 0 || txtFilter.Text == "")
+                    {
+                        /*
+                        Console.WriteLine("[Index]\t" + entry.Index +
+                                      "\n[EventID]\t" + entry.InstanceId +
+                                      "\n[TimeWritten]\t" + entry.TimeWritten +
+                                      "\n[MachineName]\t" + entry.MachineName +
+                                      "\n[Source]\t" + entry.Source +
+                                      "\n[UserName]\t" + entry.UserName +
+                                      "\n[Message]\t" + entry.Message);
+
+                        Console.WriteLine("-------------------------------------------------------------");
+                         */
+                        ListViewItem i1 = new ListViewItem("" + (listView1.Items.Count + 1));
+
+                        // Machine
+                        ListViewItem.ListViewSubItem sub_i0 = new ListViewItem.ListViewSubItem();
+                        sub_i0.Text = machine;
+                        i1.SubItems.Add(sub_i0);
+
+                        // Record Number
+                        ListViewItem.ListViewSubItem sub_i1 = new ListViewItem.ListViewSubItem();
+                        sub_i1.Text = entry.Index.ToString();
+                        i1.SubItems.Add(sub_i1);
+
+                        // DateTime information
+                        DateTime wkConditionDT = dtMax.Value;
+                        DateTime wkEntryDT = entry.TimeWritten;
+
+                        ListViewItem.ListViewSubItem sub_i2 = new ListViewItem.ListViewSubItem();
+                        sub_i2.Text = entry.TimeWritten.ToString();
+                        i1.SubItems.Add(sub_i2);
+
+                        ListViewItem.ListViewSubItem sub_i3 = new ListViewItem.ListViewSubItem();
+                        sub_i3.Text = entry.Source;
+                        i1.SubItems.Add(sub_i3);
+
+                        ListViewItem.ListViewSubItem sub_i4 = new ListViewItem.ListViewSubItem();
+                        string sMsg = entry.Message;
+                        sMsg = sMsg.Replace(@"中事件 ID '0' 的描述。本機電腦可能沒有所需的登錄資訊或訊息 DLL 檔案，因此無法顯示訊息，或者您可能沒有存取的使用權限。", "");
+                        sub_i4.Text = sMsg.Replace(@"找不到來源", "");
+                        i1.SubItems.Add(sub_i4);
+
+                        if (DateTime.Compare(wkConditionDT, wkEntryDT) >= 0)
+                        {
+                            listView1.Items.Add(i1);
+                        }
+
+                        pgBar.Value = listView1.Items.Count;
+
+                    }
+                }
+            }
+            cmdQuery.Enabled = true;
+
+        }
+
+        private void cmdQuery_Click(object sender, EventArgs e)
+        {
+            cmdQuery.Enabled = false;
+
+            Thread backgroundThread = new Thread(
+        new ThreadStart(() =>
+        {
+
+
+            this.Invoke((MethodInvoker)delegate()
+            {
+
+                listView1.Items.Clear();
+                gatherEvent(ddlServer.Text, txtFilter.Text.Trim(), ddlEventType.SelectedItem.ToString().Trim());
+            });
+            //performSearch();
+        }
+    ));
+            backgroundThread.Start();
+
+
+
+        }
+
+        private void cmdExit_Click(object sender, EventArgs e)
+        {
+            isExit = true;
+            if (System.Windows.Forms.Application.MessageLoop)
+            {
+                // Use this since we are a WinForms app
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                // Use this since we are a console app
+                System.Environment.Exit(1);
+            }
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.CopyListViewToClipboard(listView1);
+        }
+
+        public void CopyListViewToClipboard(ListView lv)
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            for (int i = 0; i < lv.Columns.Count; i++)
+            {
+                buffer.Append(lv.Columns[i].Text);
+                buffer.Append("\t");
+            }
+
+            buffer.Append(System.Environment.NewLine);
+            for (int i = 0; i < lv.Items.Count; i++)
+            {
+                for (int j = 0; j < lv.Columns.Count; j++)
+                {
+                    buffer.Append(lv.Items[i].SubItems[j].Text);
+                    buffer.Append("\t");
+                }
+
+                buffer.Append(System.Environment.NewLine);
+                buffer.Append("====================================================================");
+                buffer.Append(System.Environment.NewLine);
+            }
+
+            Clipboard.SetText(buffer.ToString());
+        }
+    }
+}
